803cc1f1119efb6f6551d278aba486ce87e6b5e7
cyril0124 <211998078@qq.com>
2024-06-26 14:10:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git submodule update --init --recursive
+
 git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.sh
 
@@ -9,3 +11,5 @@ git clone https://github.com/microsoft/vcpkg
 
 ./vcpkg/vcpkg install boost-stacktrace
 ./vcpkg/vcpkg install libbacktrace
+
+./vcpkg/vcpkg install argparse
